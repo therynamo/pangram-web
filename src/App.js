8a801340f0b21f6e21c273c35f870f937cb741e2
therynamo <groetkentheryn@gmail.com>
2019-07-23 16:20:10 -0500

    Init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import fetch from "unfetch";
+import "./App.css";
 
 function App() {
+  const [count, setCount] = useState();
+
+  useEffect(() => {
+    const fetchCount = async () => {
+      let pangramCount = 0;
+
+      try {
+        const res = await fetch(
+          "https://us-central1-development-246523.cloudfunctions.net/pangram"
+        );
+
+        pangramCount = await res.json();
+      } catch (e) {
+        console.log(e);
+      }
+
+      console.log(pangramCount);
+      setCount(pangramCount.count);
+    };
+
+    fetchCount();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>{count}</code> pangram{count > 1 ? "s" : ""} today!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.nytbee.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          View the answers at nytbee.com
         </a>
       </header>
     </div>
